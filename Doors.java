cd70f463db74710d307a3d1f4a5ea3d2dd0954f9
Vilyan01 <bheller2010@live.com>
2013-06-03 19:45:46 -0600

    Create Doors.java

---
+++ Doors.java
@@ -0,0 +1,30 @@
+
+public class Door {
+  	private String name;
+		private boolean secured;
+		
+		public Door(String name) {
+			this.name = name;
+			secured = true;
+		}
+		
+		public boolean isSecured() {
+			return secured;
+		}
+		
+		public void close() {
+			secured = true;
+		}
+		
+		public void open() {
+			secured = false;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String newName) {
+			this.name = newName;
+		}
+	}
